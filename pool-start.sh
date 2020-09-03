53674fd25dbb3b46aa82dc6e7a6973cccf57d125
Slashbin Slashnoname <slashbinslashnoname@gmail.com>
2020-09-04 00:32:41 +0400

    Update pool-start.sh

--- pool-start.sh
+++ pool-start.sh
@@ -5,13 +5,14 @@ export NVM_DIR="$HOME/.nvm"
 echo ""
 echo "Raven Pool Starting..."
 echo ""
-cd ~/rvn-kawpow-pool
+cd /app
+
 source ~/.bashrc
 source /etc/os-release
 
 ## who am i? ##
 SCRIPTNAME="$(readlink -f ${BASH_SOURCE[0]})"
-BASEDIR="~/rvn-kawpow-pool"
+BASEDIR="/app"
 
 ## Okay, print it ##
 echo "Script name : $SCRIPTNAME"
@@ -20,13 +21,9 @@ echo "Script location path (dir) : $BASEDIR"
 echo ""
 sudo setcap 'cap_net_bind_service=+ep' ~/.nvm/versions/node/v8.1.4/bin/node
 
-screen -S pool node init.js
+node init.js
 
 renice -n -18 -p $(pidof node)
 renice -n -18 -p $(pidof nodejs)
-
-echo ""
-echo "Done!"
-echo ""
 
 exit 0
